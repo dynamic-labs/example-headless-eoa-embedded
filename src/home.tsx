bf6ded17a5b699be7caed82720299672f0d50431
Yoni Goldberg <yoni@dynamic.xyz>
2024-08-02 16:43:11 -0700

    add headless verify user example

--- src/home.tsx
+++ src/home.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
 
-import { useUserWallets, useRefreshUser } from "@dynamic-labs/sdk-react-core";
 import {
   EmbeddedWalletChainEnum,
   EmbeddedWalletProviderEnum,
+  VerifyRequestFromJSON
 } from "@dynamic-labs/sdk-api-core";
+
+import {useUserWallets, useRefreshUser} from '@dynamic-labs/sdk-react-core';
+import { generateMessageToSign } from '@dynamic-labs/multi-wallet';
+import { mainnet } from "viem/chains";
+import {createWalletClient, http} from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
 
 import { DYNAMIC_ENVIRONMENT_ID } from "./constants";
 import { dynamicApi } from "./api";
@@ -12,6 +18,78 @@ import { dynamicApi } from "./api";
 export const Home: FC = () => {
   const refreshUser = useRefreshUser();
   const userWallets = useUserWallets();
+
+  const consumeNonce = () => {
+    const nonceString = localStorage['dynamic_nonce'];
+    if (nonceString) {
+      const nonceObject= JSON.parse(nonceString)
+      const nonce = nonceObject.value;
+      localStorage.removeItem('dynamic_nonce');
+      return nonce;
+    }
+  }
+
+  const genTestWallet = () => {
+    const privateKey = generatePrivateKey();
+    const account = privateKeyToAccount(privateKey);
+    return createWalletClient({
+      account,
+      chain: mainnet,
+      transport: http()
+    });
+  }
+
+  const callVerifyUser = async (messageToSign: string, address: string, signedMessage: string) => {
+    const verifyRequest = VerifyRequestFromJSON({
+      chain: 'EVM',
+      messageToSign,
+      network: "1",
+      publicWalletAddress: address,
+      signedMessage,
+      walletName: 'unknown',
+      walletProvider: 'browserExtension'
+    });
+
+    try {
+      const response = await dynamicApi().verify({
+        environmentId: DYNAMIC_ENVIRONMENT_ID,
+        verifyRequest,
+      });
+      console.log(response);
+      window.localStorage.setItem(
+        'dynamic_authentication_token',
+        JSON.stringify(response.jwt),
+      );
+
+      window.localStorage.setItem(
+        'dynamic_min_authentication_token',
+        JSON.stringify(response.jwt),
+      );
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+  const handleVerifyWallet = async () => {
+    const wallet = genTestWallet();
+    const [address] = await wallet.getAddresses()
+    const nonce = consumeNonce();
+    console.log({address, nonce})
+
+    const messageToSign = generateMessageToSign({
+      blockchain: 'EVM',
+      chainId: 1,
+      domain: window.location.host,
+      nonce,
+      publicKey: address,
+      requestId: DYNAMIC_ENVIRONMENT_ID,
+      uri: window.location.href,
+    })
+
+    const signedMessage = await wallet.signMessage({message : messageToSign});
+    await callVerifyUser(messageToSign, address, signedMessage);
+    refreshUser();
+  }
 
   const handleCreateEmbeddedWallet = async () => {
     // Call Dynamic API to create an embedded wallet
@@ -43,20 +121,23 @@ export const Home: FC = () => {
 
   return (
     <div style={{ padding: '30px'}}>
-      <div>
-        <button onClick={handleCreateEmbeddedWallet}>
-          Create embedded wallet
-        </button>
-        <button onClick={handleSignMessage}>
-          Sign message w/ embedded wallet
-        </button>
-      </div>
-      <h3>Branded Wallet</h3>
-      <pre>{JSON.stringify(userWallets.find((w) => w.key !== 'turnkeyhd'), null, 4)}</pre>
-      <h3>Embedded Wallet</h3>
-      <pre>{JSON.stringify(userWallets.find((w) => w.key === 'turnkeyhd'), null, 4)}</pre>
-      <h3>All Wallets</h3>
-      <pre>{JSON.stringify(userWallets, null, 4)}</pre>
+    <div>
+    <button onClick={handleVerifyWallet}>
+    Verify EOA wallet
+    </button>
+    <button onClick={handleCreateEmbeddedWallet}>
+    Create embedded wallet
+    </button>
+    <button onClick={handleSignMessage}>
+    Sign message w/ embedded wallet
+    </button>
+    </div>
+    <h3>Branded Wallet</h3>
+    <pre>{JSON.stringify(userWallets.find((w) => w.key !== 'turnkeyhd'), null, 4)}</pre>
+    <h3>Embedded Wallet</h3>
+    <pre>{JSON.stringify(userWallets.find((w) => w.key === 'turnkeyhd'), null, 4)}</pre>
+    <h3>All Wallets</h3>
+    <pre>{JSON.stringify(userWallets, null, 4)}</pre>
     </div>
   );
 };
